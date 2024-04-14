256f69252ca08de19abd0361621ac56313dbb502
任懷林 <renhuailin@gmail.com>
2024-04-14 23:02:41 +0800

    update

--- prettier.config.js
+++ prettier.config.js
@@ -1,5 +1,5 @@
 const styleguide = require('@vercel/style-guide/prettier');
-
+styleguide['tabWidth'] = 4
 module.exports = {
   ...styleguide,
   plugins: [...styleguide.plugins, 'prettier-plugin-tailwindcss'],
